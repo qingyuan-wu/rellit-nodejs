cda79f06260d3eaef5f06e975cf3b4a72f4b7662
Qingyuan Wu <qyw.wu@mail.utoronto.ca>
2022-07-16 15:40:57 +0000

    store user's coords upon login

--- views/scripts/signin.js
+++ views/scripts/signin.js
@@ -1,21 +1,24 @@
 function handleCredentialResponse(res) {
-    let resJson = jwt_decode(res.credential);
+    
     // resJson["coords"] = coords;
     navigator.geolocation.getCurrentPosition(position => {
+        let resJson = jwt_decode(res.credential);
         const latitude  = position.coords.latitude;
         const longitude = position.coords.longitude;
+        console.log(latitude);
+        console.log("hellooooo");
         resJson.long = longitude;
         resJson.lat = latitude;
+        resJson.test = "hello";
+        console.log(resJson);
+
+        document.getElementById("custom-greeting").innerHTML = `Hello ${resJson.given_name}!`;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/login", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(resJson));
     })
-    resJson.test = "hello";
-    console.log(resJson);
-
-    document.getElementById("custom-greeting").innerHTML = `Hello ${resJson.given_name}!`;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/login", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(resJson));
 }
 
 function getCoordinates() {
