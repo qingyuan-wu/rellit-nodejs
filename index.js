618aef0cccbcb4291c18210c25eef387b5aa73f0
Raymond Feckoury <rjf21434@uga.edu>
2022-07-19 01:24:42 +0000

    Add an endpoint that starts a session and directs the user to the questions page.

--- index.js
+++ index.js
@@ -24,26 +24,49 @@ app.use(express.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Starts a session and directs the user to the questions page
 app.post('/start-session', async (req, res) => {
     app.use(session({
         secret: 'secret-key',
         resave: false,
         saveUninitialized: false,
     }));
-});
 
-app.use(session({
-    secret: 'secret-key',
-    resave: false,
-    saveUninitialized: false,
-}));
+    const queryGetQuestions = datastore
+    .createQuery("Question")
+    .order("time", {
+        descending: true
+    });
+    const [out] = await datastore.runQuery(queryGetQuestions);
+    const questions = await Promise.all(out.map(async q => { 
+        const questionId = q[datastore.KEY].id;
+        const queryGetReplies = datastore.createQuery("Reply")
+        .filter("questionId", "=", questionId)
+        .order("time", {
+            descending: true
+        });
+
+        const [replies] = await datastore.runQuery(queryGetReplies);
+            
+        return {
+            "questionId": questionId,
+            "text": q.text,
+            "time": q.time,
+            "replies": replies,
+        };   
+    }));
+    
+    var sessionStatus = "Session Started!";
+
+    res.render('index', { data: questions, default: req.query.question, sessionStuff: sessionStatus});
+});
 
 app.get('/', async (req, res) => {
     var sessionStatus;
     if (req.session) {
-        sessionStatus = "Welcome!"
+        sessionStatus = "Session Started!"
     } else {
-        sessionStatus = "Sign in to leave a comment!";
+        sessionStatus = "Session is not active";
     }
     const queryGetQuestions = datastore
     .createQuery("Question")
@@ -70,7 +93,7 @@ app.get('/', async (req, res) => {
     }));
     
 
-  res.render('index', { data: questions, default: req.query.question, sessionStuff: sessionStatus});
+    res.render('index', { data: questions, default: req.query.question, sessionStuff: sessionStatus});
 });
 
 app.get('/questions', async (req, res) => {
@@ -160,7 +183,13 @@ app.post('/login', async (req, res) => {
 });
 
 app.get("/meet", (req, res) => {
-    return res.render("meet");
+    var sessionStatus;
+    if (req.session) {
+        sessionStatus = "session active"
+    } else {
+        sessionStatus = "session not active";
+    }
+    return res.render("meet", {sessionStuff: sessionStatus });
 });
 
 app.listen(port, () => {
